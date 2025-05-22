7311ffab4deca787e37716a0d5034a7b31af7498
Raushan Ruslan kyzy <raushanruslankyzy@Raushans-MacBook-Pro.local>
2025-05-22 08:58:32 +0200

    Initial commit with basic calculator and virtual environment ignored

---
+++ calculator/main.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+operator = input("Enter operator: ")
+num2 = int(input("Enter second number: "))
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
